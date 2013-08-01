36499ac1760f67422497af0b9e272a714657a6b8
Joel Shin <jwshin@gmail.com>
2013-07-31 20:20:47 -0700

    maybe fixed category order-by-access-count bug?

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -14,15 +14,26 @@ describe Category do
 
   describe "all by access count" do
     subject { Category }
-    its(:all_by_access_count) { should == Category.all(:order => 'access_count DESC') }
-  end
+    #its(:all_by_access_count) { should == Category.all(:order => 'access_count DESC') }
+    its(:by_access_count) { should == Category.all(:order => 'access_count DESC') }
+  end                                                                                  
 
   describe 'articles by access count' do
     subject { category }
     its(:articles_by_access_count) { should == category.articles.all(:order => 'access_count DESC') }
   end
-
-
+                                          
+  describe "all by access count" do
+   before do
+     FactoryGirl.create(:aaa_name_with_low_access_count)
+     FactoryGirl.create(:aaa_name_with_high_access_count)
+     FactoryGirl.create(:zzz_name_with_low_access_count)
+     FactoryGirl.create(:zzz_name_with_high_access_count)
+   end
+   it "returns by greatest first" do
+     Category.by_access_count.all.should == Category.reorder('access_count desc').all
+   end                                     
+  end
 
 end
 # == Schema Information
